46eae14a3e6a111a2f2ccdba67f937974671f98a
kiran <kiranugale239@gmail.com>
2024-09-02 20:38:14 +0530

    sign-in api modified

--- src/app/api/sign-in/route.js
+++ src/app/api/sign-in/route.js
@@ -1,3 +1,8 @@
+import jwt from "jsonwebtoken";
+import User from "@/model/User";
+import bcrypt from "bcryptjs";
+import DatabaseConn from "@/database/DatabaseCon";
+import { NextResponse } from "next/server";
 export async function POST(req) {
   try {
     await DatabaseConn();
@@ -13,7 +18,6 @@ export async function POST(req) {
     //remove whitespace
     email = email.trim();
     password = password.trim();
-    console.log(email);
 
     //check user email is exit
     const userExit = await User.findOne({ email: email });
@@ -47,61 +51,3 @@ export async function POST(req) {
     });
   }
 }
-const User = require("../model/User.js");
-const DatabaseConn = require("../database/DatabaseCon.js");
-const jwt = require("jsonwebtoken");
-
-const VerifyOtpHandler = async (req, res, next) => {
-  await DatabaseConn();
-  try {
-    let { email, otp } = req.body;
-
-    // Find the user
-    const user = await User.findOne({ email: email });
-    otp = otp.trim();
-    if (!user) {
-      return res.json({
-        success: false,
-        message: "This Email is Not found try again!",
-      });
-    }
-
-    const token = jwt.sign(
-      { email, userId: user._id },
-      process.env.TOKEN_SECRET_KEY,
-      { expiresIn: "24h" }
-    );
-
-    // Check if OTP is correct and not expired
-
-    if (user.otp === otp && Date.now() < user.expiration) {
-      await User.updateOne(
-        { email: email },
-        { verifyUser: true, token: token }
-      );
-      return res.json({
-        success: true,
-        message: "Email verified",
-        user: user,
-        token: token,
-      });
-    } else if (Date.now() >= user.expiration) {
-      return res.json({
-        success: false,
-        message: "otp exprise",
-      });
-    } else {
-      return res.json({
-        success: false,
-        message: "Invalid OTP",
-      });
-    }
-  } catch (error) {
-    console.log(error.message);
-
-    return res.json({
-      success: false,
-      message: "Server problem",
-    });
-  }
-};
