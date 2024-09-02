082ab809580b00377fbdf9e2e9450636adbfe3ec
kiran <kiranugale239@gmail.com>
2024-09-02 20:38:55 +0530

    sign-up page  modified

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 export default async function SignUp() {
   return (
     <>
-      <div className="p-auto lg:p-24">
+      <div className="p-auto lg:p-20">
         <SignUPCard />
       </div>
     </>
